52013b61194924fa0a50989cd7b6140e24e0c33d
Alex <alex.soderberg97@gmail.com>
2021-04-13 17:56:10 +0200

    adds example environment file, and cleaner code

---
+++ src/environments/environmentExample.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiKey: "YOUR_API_KEY_HERE",
+    apiUrlRandom: "https://api.spoonacular.com/recipes/complexSearch?query=vegetarian&number=10&",
+    filteredUrl: "https://api.spoonacular.com/recipes/complexSearch?",
+    noGluten: "intolerances=Gluten&",
+    noSoy: "intolerances=Soy&",
+    noPeanuts: "intolerances=Peanuts&"
+  
+  };
